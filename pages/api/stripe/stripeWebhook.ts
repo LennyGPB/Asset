eac7f90252c546ab9c19e78866829a93849c28c1
Lenny Gomes <lennygomes.hp@gmail.com>
2024-10-29 11:04:08 +0100

    stripe8

--- pages/api/stripe/stripeWebhook.ts
+++ pages/api/stripe/stripeWebhook.ts
@@ -4,8 +4,8 @@ import { prisma } from '@/lib/prisma';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-    apiVersion: '2024-09-30.acacia', // Version de l'API Stripe
-  });
+  apiVersion: '2024-06-20',
+});
 
 // Désactive le bodyParser pour cette route spécifique
 export const config = {
